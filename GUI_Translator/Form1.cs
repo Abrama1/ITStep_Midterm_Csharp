efaa8ea648e569b3acc6c3ae6fbd743f71132d41
Davit Abramishvili <david.abramishvili10@gmail.com>
2024-11-24 18:23:04 +0400

    working with httpclient to translate text which returns translationResult if response is 200

--- GUI_Translator/Form1.cs
+++ GUI_Translator/Form1.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +15,32 @@ namespace GUI_Translator
 {
     public partial class Form1 : Form
     {
-        string firstString, secondString;
+        private const string baseUrl = "http://api.mymemory.translated.net";
+        private HttpClient httpClient;
+        
+
         public Form1()
         {
             InitializeComponent();
+            httpClient = new HttpClient();
+        }
+
+        private async Task<string> TranslateAsync(string text, string sourceLang, string targetLang)
+        {
+            string url = $"{baseUrl}/get?q={Uri.EscapeDataString(text)}&langpair={sourceLang}|{targetLang}";
+
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string responseJson = await response.Content.ReadAsStringAsync();
+            var translationResult = JsonConvert.DeserializeObject<TranslationResponse>(responseJson);
+
+            if (translationResult.Response == 200)
+            {
+                return translationResult.TranslatedText;
+            }
+
+            return string.Empty;
         }
 
         private void Form1_Load(object sender, EventArgs e)
